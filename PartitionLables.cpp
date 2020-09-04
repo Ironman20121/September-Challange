ae2a66dc45bbbe027b09fc86ed55bc59477fec7d
Sai Kundan Suddapalli <46628243+Ironman20121@users.noreply.github.com>
2020-09-04 21:17:23 +0530

    Update PartitionLables.cpp
    trying for perfection

--- PartitionLables.cpp
+++ PartitionLables.cpp
@@ -1,37 +1,51 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-using namespace std;
+/*
+A string S of lowercase English letters is given.We want to partition this string into as many parts as possible so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
+Example 1:
+Input: S = "ababcbacadefegdehijhklij"
+Output : [9, 7, 8]
+Explanation :
+	The partition is "ababcbaca", "defegde", "hijhklij".
+	This is a partition so that each letter appears in at most one part.
+	A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
+
+*/
+//Solutions
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+//using namespace std;
 class Solution {
 public:
-	vector<int>pl(string s) {
-		map<char, int>m;
+    std::vector<int> partitionLabels(std::string s) {
+        std::map<char, int> m;
         for (int i = 0; i < s.size(); i++)
             m[s[i]] = i;
-        int j = 0, start = 0;
-        int i = 0;
-        int n = s.size();
-        vector <int> ans;
-        while (i < n) {
-            j = max(j, m[s[i]]);
-           
+        int i = 0, j = 0, start = 0;
+        std::vector<int>a;
+        while (i < s.size()) {
+            j = std::max(j, m[s[i]]);
             if (i == j) {
-                ans.push_back(i - start + 1);
-                start = ++i;
+                a.push_back(i - start + 1);
+                start = i + 1;
             }
             i++;
-        }
-        return ans;
-	
-	}
 
- };
+        }
+        
+            return a;
+    }
+};
 
 int main() {
-	Solution obj;
-	vector<int>v = obj.pl("ababcbacadefegdehijhklij");
-    for (int i = 0; i < v.size(); i++) {
-        cout << v[i] << ", ";
-    }
+    Solution obj;
+    std::string s; 
+   //s = "ababcbacadefegdehijhklij";
+    std::cin >> s;
+    std::vector<int>ans = obj.partitionLabels(s);
+    for (auto a : ans)
+        std::cout << a<<" ";
+  
+    
 }
